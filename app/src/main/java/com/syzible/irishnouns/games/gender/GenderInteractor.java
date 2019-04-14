1b868420edf4c6ff8c53170121e29b594d339594
Edmond O' Flynn <oflynned@tcd.ie>
2019-04-14 16:22:19 +0100

    add noun interactor

--- app/src/main/java/com/syzible/irishnouns/games/gender/GenderInteractor.java
+++ app/src/main/java/com/syzible/irishnouns/games/gender/GenderInteractor.java
@@ -1,4 +1,55 @@
 package com.syzible.irishnouns.games.gender;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.syzible.irishnouns.common.models.Noun;
+import com.syzible.irishnouns.common.persistence.API;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenderInteractor {
+
+    public List<String> fetchDomains() throws MalformedFileException {
+        JSONArray domains;
+        try {
+            domains = API.domainStore();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+            throw new MalformedFileException();
+        }
+
+        List<String> domainList = new ArrayList<>();
+        for (int i = 0; i < domains.length(); i++) {
+            try {
+                String domain = domains.getString(i);
+                domainList.add(domain);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return domainList;
+    }
+
+    public List<Noun> fetchNouns(String domain) throws DomainNotFoundException, MalformedFileException {
+        JSONArray response;
+        try {
+            response = API.nounStore(domain);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new DomainNotFoundException();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new MalformedFileException();
+        }
+
+        return new Gson().fromJson(response.toString(),
+                new TypeToken<List<Noun>>() {
+                }.getType());
+    }
 }
