ea02d6f1cb49c9fa44a883e981f63bd35c7b1214
Edmond O' Flynn <oflynned@tcd.ie>
2019-04-28 17:43:28 +0200

    add cache helper

--- app/src/main/java/com/syzible/irishnouns/games/Cache.java
+++ app/src/main/java/com/syzible/irishnouns/games/Cache.java
@@ -1,13 +1,29 @@
-package com.syzible.irishnouns.common.persistence;
+package com.syzible.irishnouns.games;
 
 import android.content.Context;
 
-public class Cache{
+import com.syzible.irishnouns.common.persistence.DomainNotFoundException;
+import com.syzible.irishnouns.common.persistence.LocalStorage;
+
+public class Cache {
     public static void setNewHighScore(Context context, int value) {
         LocalStorage.setIntegerPref(context, LocalStorage.Pref.HIGH_SCORE, value);
     }
 
     public static int getHighScore(Context context) {
         return LocalStorage.getIntegerPref(context, LocalStorage.Pref.HIGH_SCORE);
+    }
+
+    public static void setLastChosenCategory(Context context, String category) {
+        LocalStorage.setStringPref(context, LocalStorage.Pref.LAST_CHOSEN_CATEGORY, category);
+    }
+
+    public static String getLastChosenCategory(Context context) throws DomainNotFoundException {
+        String lastChosenCategory = LocalStorage.getStringPref(context, LocalStorage.Pref.LAST_CHOSEN_CATEGORY);
+        if (lastChosenCategory == null) {
+            throw new DomainNotFoundException();
+        }
+
+        return lastChosenCategory;
     }
 }
