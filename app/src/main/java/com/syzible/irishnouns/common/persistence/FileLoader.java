7ccf486b93f3e03917d60eb26f60d2dacb9b005e
Edmond O' Flynn <oflynned@tcd.ie>
2019-05-20 19:22:29 +0100

    change file loader logic to use a string builder to improve efficiency

--- app/src/main/java/com/syzible/irishnouns/common/persistence/FileLoader.java
+++ app/src/main/java/com/syzible/irishnouns/common/persistence/FileLoader.java
@@ -3,8 +3,10 @@ package com.syzible.irishnouns.common.persistence;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
@@ -12,9 +14,14 @@ class FileLoader {
     static JSONArray loadFile(String path) throws IOException, JSONException {
         InputStream is = Objects.requireNonNull(FileLoader.class.getClassLoader())
                 .getResourceAsStream("assets/" + path);
-        byte[] buffer = new byte[is.available()];
-        is.read(buffer);
-        is.close();
-        return new JSONArray(new String(buffer, StandardCharsets.UTF_8));
+        InputStreamReader inputStreamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(inputStreamReader, 8);
+        StringBuilder builder = new StringBuilder();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line).append("\n");
+        }
+        return new JSONArray(builder.toString());
     }
 }
