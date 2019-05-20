f1675e33a92ea9df2ce40099396aba3192284b4e
Edmond O' Flynn <oflynned@tcd.ie>
2019-05-20 17:11:51 +0100

    add domain choice view

--- app/src/main/java/com/syzible/irishnouns/games/domainchoice/DomainChoiceView.java
+++ app/src/main/java/com/syzible/irishnouns/games/domainchoice/DomainChoiceView.java
@@ -1,4 +1,10 @@
 package com.syzible.irishnouns.games.domainchoice;
 
-public interface DomainChoiceView {
+import com.hannesdorfmann.mosby3.mvp.MvpView;
+import com.syzible.irishnouns.common.models.Category;
+
+import java.util.List;
+
+interface DomainChoiceView extends MvpView {
+    void showCategoryList(List<Category> categoryList);
 }
