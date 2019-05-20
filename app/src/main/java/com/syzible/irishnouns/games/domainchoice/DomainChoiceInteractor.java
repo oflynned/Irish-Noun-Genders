19735180fb4bc6d22ac566734d86b0c3fa1dbccc
Edmond O' Flynn <oflynned@tcd.ie>
2019-05-20 17:11:40 +0100

    add domain choice interactor

--- app/src/main/java/com/syzible/irishnouns/games/domainchoice/DomainChoiceInteractor.java
+++ app/src/main/java/com/syzible/irishnouns/games/domainchoice/DomainChoiceInteractor.java
@@ -1,4 +1,34 @@
 package com.syzible.irishnouns.games.domainchoice;
 
-public class DomainChoiceInteractor {
+import android.content.Context;
+
+import com.syzible.irishnouns.common.persistence.API;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class DomainChoiceInteractor {
+    interface DomainCallback {
+        void onSuccess(Context context, List<String> domainList);
+
+        void onFailure(String message);
+    }
+
+    void fetchDomains(Context context, DomainCallback callback) throws JSONException, IOException {
+        List<String> categoryList = new ArrayList<>();
+        JSONArray domains = API.domainStore();
+
+        for (int i = 0; i < domains.length(); i++) {
+            String domain = domains.getString(i);
+            categoryList.add(domain);
+        }
+
+        Collections.sort(categoryList);
+        callback.onSuccess(context, categoryList);
+    }
 }
