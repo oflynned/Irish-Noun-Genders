9efb03ff0c5a5365f24f6b79d7dda0e1e8048d2a
Edmond O' Flynn <oflynned@tcd.ie>
2019-05-20 17:11:45 +0100

    add domain choice presenter

--- app/src/main/java/com/syzible/irishnouns/games/domainchoice/DomainChoicePresenter.java
+++ app/src/main/java/com/syzible/irishnouns/games/domainchoice/DomainChoicePresenter.java
@@ -1,4 +1,50 @@
 package com.syzible.irishnouns.games.domainchoice;
 
-public class DomainChoicePresenter {
+import android.content.Context;
+
+import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
+import com.syzible.irishnouns.common.models.Category;
+import com.syzible.irishnouns.common.persistence.LocalStorage;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class DomainChoicePresenter extends MvpBasePresenter<DomainChoiceView>
+        implements DomainChoiceInteractor.DomainCallback {
+
+    private DomainChoiceInteractor interactor;
+
+    DomainChoicePresenter() {
+        interactor = new DomainChoiceInteractor();
+    }
+
+    void fetchCategories(Context context) {
+        try {
+            interactor.fetchDomains(context, this);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSuccess(Context context, List<String> domainList) {
+        List<Category> categoryList = new ArrayList<>();
+        String currentCategory = LocalStorage.getStringPref(context, LocalStorage.Pref.CURRENT_CATEGORY);
+        for (String domain : domainList) {
+            Category category = new Category(domain, currentCategory);
+            categoryList.add(category);
+        }
+
+        ifViewAttached(v -> v.showCategoryList(categoryList));
+    }
+
+    @Override
+    public void onFailure(String message) {
+
+    }
 }
