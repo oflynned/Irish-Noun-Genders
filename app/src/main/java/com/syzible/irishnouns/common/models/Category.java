2d701076ecd730174d3d02a5cf1a6c6a494ec6cc
Edmond O' Flynn <oflynned@tcd.ie>
2019-05-20 17:08:41 +0100

    add file name to category pojo

--- app/src/main/java/com/syzible/irishnouns/common/models/Category.java
+++ app/src/main/java/com/syzible/irishnouns/common/models/Category.java
@@ -1,4 +1,30 @@
 package com.syzible.irishnouns.common.models;
 
 public class Category {
+    private String displayName, fileName;
+    private boolean isChosen;
+
+    public Category(String displayName, String currentCategory) {
+        this.displayName = displayName;
+        this.fileName = domainToFile(displayName);
+        this.isChosen = getFileName().equals(currentCategory);
+    }
+
+    public String getCategory() {
+        return displayName.toLowerCase();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public boolean isChosen() {
+        return isChosen;
+    }
+
+    public static String domainToFile(String domain) {
+        return domain.toLowerCase()
+                .replace(" ", "_")
+                .replace("&", "and");
+    }
 }
