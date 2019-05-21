6018a06d629d4ff51d65b3aad67ad138d43ddc25
Edmond O' Flynn <oflynned@tcd.ie>
2019-05-21 15:19:56 +0100

    add fragment impl

--- app/src/main/java/com/syzible/irishnouns/mainmenu/MainMenuFragment.java
+++ app/src/main/java/com/syzible/irishnouns/mainmenu/MainMenuFragment.java
@@ -1,4 +1,55 @@
 package com.syzible.irishnouns.mainmenu;
 
-public class MainMenuFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.syzible.irishnouns.MainActivity;
+import com.syzible.irishnouns.R;
+import com.syzible.irishnouns.games.gender.GenderFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class MainMenuFragment extends Fragment {
+
+    private Unbinder unbinder;
+
+    @BindView(R.id.main_menu_gender_mode)
+    TextView genderMode;
+
+    @BindView(R.id.main_menu_settings)
+    TextView settings;
+
+    public MainMenuFragment() {
+    }
+
+    public static MainMenuFragment getInstance() {
+        return new MainMenuFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+        genderMode.setOnClickListener(v -> MainActivity.setFragment(getFragmentManager(), GenderFragment.getInstance()));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
