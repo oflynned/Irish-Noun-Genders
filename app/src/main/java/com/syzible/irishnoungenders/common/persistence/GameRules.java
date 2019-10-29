0541624e7d75b2e7c54555c2b493623cf54ae425
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-29 17:20:29 +0100

    create game rules wrapper

--- app/src/main/java/com/syzible/irishnoungenders/common/persistence/GameRules.java
+++ app/src/main/java/com/syzible/irishnoungenders/common/persistence/GameRules.java
@@ -1,4 +1,9 @@
 package com.syzible.irishnoungenders.common.persistence;
 
+import android.content.Context;
+
 public class GameRules {
+    public static boolean wordHintsEnabled(Context context) {
+        return LocalStorage.getBooleanPref(context, LocalStorage.Pref.SHOW_HINTS);
+    }
 }
