dca7da38b4c1974f9eb426f731ca59df05fb33fa
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-29 17:21:35 +0100

    set settings pref values

--- app/src/main/java/com/syzible/irishnoungenders/screens/options/settings/SettingsFragment.java
+++ app/src/main/java/com/syzible/irishnoungenders/screens/options/settings/SettingsFragment.java
@@ -2,9 +2,13 @@ package com.syzible.irishnoungenders.screens.options.settings;
 
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.syzible.irishnoungenders.R;
+import com.syzible.irishnoungenders.common.persistence.GameRules;
+import com.syzible.irishnoungenders.common.persistence.LocalStorage;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     public SettingsFragment() {
@@ -15,7 +19,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     }
 
     @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
-        addPreferencesFromResource(R.xml.settings_preferences);
+    public void onCreatePreferences(Bundle bundle, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_preferences, rootKey);
+
+        Preference buildVersion = findPreference("settings_build_version");
+        buildVersion.setSummary(getString(R.string.made_in_ireland, getString(R.string.app_version)));
+
+        SwitchPreferenceCompat nounHints = findPreference("settings_show_hints");
+        nounHints.setChecked(GameRules.wordHintsEnabled(getContext()));
+        nounHints.setOnPreferenceChangeListener((preference, newValue) -> {
+            LocalStorage.setBooleanPref(getContext(), LocalStorage.Pref.SHOW_HINTS, (Boolean) newValue);
+            return true;
+        });
     }
 }
