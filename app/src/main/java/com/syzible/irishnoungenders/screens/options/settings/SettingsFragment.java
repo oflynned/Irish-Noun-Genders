b6285f3a606d50814d9144115ca05ccedd029d76
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-29 20:43:14 +0100

    hide how to play and simplify version code retrieval

--- app/src/main/java/com/syzible/irishnoungenders/screens/options/settings/SettingsFragment.java
+++ app/src/main/java/com/syzible/irishnoungenders/screens/options/settings/SettingsFragment.java
@@ -2,12 +2,14 @@ package com.syzible.irishnoungenders.screens.options.settings;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
+import com.syzible.irishnoungenders.BuildConfig;
 import com.syzible.irishnoungenders.R;
 import com.syzible.irishnoungenders.common.persistence.GameRules;
 import com.syzible.irishnoungenders.common.persistence.LocalStorage;
@@ -20,23 +22,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         return new SettingsFragment();
     }
 
-    private String getVersionCode() {
-        try {
-            PackageInfo pInfo = getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0);
-            return pInfo.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
     @Override
     public void onCreatePreferences(Bundle bundle, String rootKey) {
         setPreferencesFromResource(R.xml.settings_preferences, rootKey);
 
         Preference buildVersion = findPreference("settings_build_version");
-        buildVersion.setSummary(getString(R.string.made_in_ireland, getVersionCode()));
+        buildVersion.setSummary(getString(R.string.made_in_ireland, BuildConfig.VERSION_NAME));
 
         SwitchPreferenceCompat nounHints = findPreference("settings_show_hints");
         nounHints.setChecked(GameRules.wordHintsEnabled(getContext()));
