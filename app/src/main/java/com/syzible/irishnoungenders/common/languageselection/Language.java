56d5d15f0be835dba208a1c7df864674ad45fc95
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-30 17:28:03 +0100

    create base language activity with language config tools

--- app/src/main/java/com/syzible/irishnoungenders/common/languageselection/Language.java
+++ app/src/main/java/com/syzible/irishnoungenders/common/languageselection/Language.java
@@ -1,4 +1,40 @@
 package com.syzible.irishnoungenders.common.languageselection;
 
-public class Language {
+import androidx.annotation.NonNull;
+
+class Language implements Comparable<Language> {
+    private String languageName, localisedName, languageCode;
+
+    Language(String languageName, String localisedName, String languageCode) {
+        this.languageName = languageName;
+        this.localisedName = localisedName;
+        this.languageCode = languageCode;
+    }
+
+    String getLanguageName() {
+        return languageName;
+    }
+
+    String getLocalisedName() {
+        return localisedName;
+    }
+
+    String getLanguageCode() {
+        return languageCode;
+    }
+
+    @Override
+    public int compareTo(@NonNull Language language) {
+        return this.localisedName.compareTo(language.localisedName);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Language{" +
+                "languageName='" + languageName + '\'' +
+                ", localisedName='" + localisedName + '\'' +
+                ", languageCode='" + languageCode + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/syzible/irishnoungenders/common/languageselection/LocaleManager.java
+++ app/src/main/java/com/syzible/irishnoungenders/common/languageselection/LocaleManager.java
@@ -1,4 +1,68 @@
 package com.syzible.irishnoungenders.common.languageselection;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+
+import androidx.preference.PreferenceManager;
+
+import com.syzible.irishnoungenders.common.persistence.LocalStorage;
+
+import java.util.Locale;
+
+import static android.os.Build.VERSION_CODES.N;
+
 public class LocaleManager {
+
+    public static final String LANGUAGE_ENGLISH = "en";
+    public static final String LANGUAGE_IRISH = "ga";
+
+    private SharedPreferences sharedPreferences;
+
+    public LocaleManager(Context context) {
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static Locale getLocale(Resources res) {
+        Configuration config = res.getConfiguration();
+        if (Build.VERSION.SDK_INT >= N) {
+            return config.getLocales().get(0);
+        } else {
+            return config.locale;
+        }
+    }
+
+    public Context setLocale(Context c) {
+        return updateResources(c, getLanguage());
+    }
+
+    public Context setNewLocale(Context c, String language) {
+        persistLanguage(language);
+        return updateResources(c, language);
+    }
+
+    public String getLanguage() {
+        return sharedPreferences.getString(LocalStorage.Pref.DISPLAY_LANGUAGE.toString(), LANGUAGE_ENGLISH);
+    }
+
+    @SuppressLint("ApplySharedPref")
+    private void persistLanguage(String language) {
+        sharedPreferences.edit()
+                .putString(LocalStorage.Pref.DISPLAY_LANGUAGE.toString(), language)
+                .commit();
+    }
+
+    private Context updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Resources res = context.getResources();
+        Configuration config = new Configuration(res.getConfiguration());
+        config.setLocale(locale);
+        context = context.createConfigurationContext(config);
+        return context;
+    }
 }
