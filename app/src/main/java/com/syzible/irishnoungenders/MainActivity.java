1148ebd7e0f5ab8594461f979f66187f2629f90c
Edmond O'Flynn <oflynned@tcd.ie>
2019-11-01 11:07:37 +0100

    reset initial frag to main menu

--- app/src/main/java/com/syzible/irishnoungenders/MainActivity.java
+++ app/src/main/java/com/syzible/irishnoungenders/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setFragment(getSupportFragmentManager(), GenderFragment.getInstance());
+        setFragment(getSupportFragmentManager(), MainMenuFragment.getInstance());
 
         if (!LocalStorage.getBooleanPref(this, LocalStorage.Pref.FIRST_RUN_COMPLETE)) {
             setupInitialSettings();
