aaf116bb8f3f496c03637e22aefa8f416f499336
Edmond O'Flynn <oflynned@tcd.ie>
2019-11-10 19:44:30 +0100

    add endpoints

--- app/src/main/java/com/syzible/irishnoungenders/common/http/Development.java
+++ app/src/main/java/com/syzible/irishnoungenders/common/http/Development.java
@@ -1,4 +1,16 @@
 package com.syzible.irishnoungenders.common.http;
 
-public class Developnebt {
+import android.os.Build;
+
+class Development {
+    static boolean isDebugMode() {
+        return Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(Build.PRODUCT);
+    }
 }
--- app/src/main/java/com/syzible/irishnoungenders/common/http/Endpoints.java
+++ app/src/main/java/com/syzible/irishnoungenders/common/http/Endpoints.java
@@ -1,4 +1,8 @@
 package com.syzible.irishnoungenders.common.http;
 
 public class Endpoints {
+    private static final String STEM_URL = Development.isDebugMode() ? "http://10.0.2.2:3001/api" : "https://irish-nouns.syzible.com/api";
+//    private static final String STEM_URL = "https://t1-exercise.herokuapp.com";
+
+    public static final String REPORT_EXPERIMENT = STEM_URL + "/experiments";
 }
