30c9156bf091f7b3008c878b94c092f308830f89
Edmond O'Flynn <oflynned@tcd.ie>
2019-11-10 19:44:49 +0100

    add experiment interactor

--- app/src/main/java/com/syzible/irishnoungenders/screens/modes/common/interactors/ExperimentHttpHandler.java
+++ app/src/main/java/com/syzible/irishnoungenders/screens/modes/common/interactors/ExperimentHttpHandler.java
@@ -1,4 +1,31 @@
 package com.syzible.irishnoungenders.screens.modes.common.interactors;
 
-public class ExperimentHttpHandler {
+import com.loopj.android.http.BaseJsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+class ExperimentHttpHandler extends BaseJsonHttpResponseHandler<JSONObject> {
+    private ExperimentInteractor.ExperimentCallback callback;
+
+    ExperimentHttpHandler(ExperimentInteractor.ExperimentCallback callback) {
+        super();
+        this.callback = callback;
+    }
+
+    @Override
+    public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, JSONObject response) {
+        callback.onSuccess();
+    }
+
+    @Override
+    public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, JSONObject errorResponse) {
+        callback.onFailure(statusCode, errorResponse);
+    }
+
+    @Override
+    protected JSONObject parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
+        return new JSONObject(rawJsonData);
+    }
 }
