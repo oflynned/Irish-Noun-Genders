5a9c71a7f17eb74e2d7db0cda4e3013e46558326
Edmond O'Flynn <oflynned@tcd.ie>
2019-11-11 17:01:59 +0100

    add unused feature lag for showcasing screenshots

--- app/src/main/java/com/syzible/irishnoungenders/common/common/FeatureFlag.java
+++ app/src/main/java/com/syzible/irishnoungenders/common/common/FeatureFlag.java
@@ -1,10 +1,12 @@
 package com.syzible.irishnoungenders.common.common;
 
 public enum FeatureFlag {
-    EARN_ACHIEVEMENTS, VIEW_ACHIEVEMENTS, SHOW_LEADERBOARDS, SHOW_TUTORIAL_SANDBOX;
+    ENABLE_SHOWCASE_SCREENSHOT, EARN_ACHIEVEMENTS, VIEW_ACHIEVEMENTS, SHOW_LEADERBOARDS, SHOW_TUTORIAL_SANDBOX;
 
     public boolean isEnabled() {
         switch (this) {
+            // TODO set this via some properties file so that automatic screenshots can be taken
+            case ENABLE_SHOWCASE_SCREENSHOT:
             case EARN_ACHIEVEMENTS:
             case VIEW_ACHIEVEMENTS:
             case SHOW_LEADERBOARDS:
